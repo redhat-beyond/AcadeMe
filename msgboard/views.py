97545d74f2264a5144e50e0f83ac55c0110a51f7
Raz Ashtar <razashtar93@gmail.com>
2021-11-05 17:53:56 +0200

    Task 7 Done (presenting data functionality
    Task 7: Creating a view for displaying our data

--- msgboard/views.py
+++ msgboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Message
 # Create your views here.
+
+
+def board(request):
+    messages = Message.objects.order_by('-date')
+    return render(request, 'msgboard/board.html', {'messages': messages})
