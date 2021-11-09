95046cbbfed153db9302586a336db4af4fadf5af
Daniel Itkes <danielitkes@gmail.com>
2021-11-09 19:30:53 +0200

    Changing name of project in all neccesary files

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group_1_demo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AcadeMe.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
