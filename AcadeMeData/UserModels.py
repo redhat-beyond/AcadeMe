5f0e7e772fc20d8efcf872d38886b5bc20ee1be6
DanWaldman <dan.waldman14@gmail.com>
2021-11-14 19:40:21 +0200

    Adding Model file for Users databse

---
+++ AcadeMeData/UserModels.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+
+class DEGREECHOICES(models.TextChoices):
+    Computer_Science = 'CS', 'Computer Science'
+    Psychology = 'PS', 'Psychology'
+    GOVERNMENT = 'GV', 'GOVERNMENT'
+    Business_Administration = 'BA', 'Business Administration'
+    Unknown = 'UN', 'Unknown'
+
+
+class TYPECHOICES(models.TextChoices):
+    Student = 'S', 'Student'
+    Expert = 'E', 'Expert'
+
+
+class UNIVERSITYCHOICES(models.TextChoices):
+    Reichman_University = 'RU', 'Reichman University'
+    Hebrew_University = 'HU', 'Hebrew University'
+    Tel_Aviv_University = 'TA', 'Tel Aviv University'
+    Beer_Sheva_University = 'BS', "Be'er Sheva University"
+    Unknown = 'UN', 'Unknown'
+
+
+class Users(models.Model):
+    ID = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    Name = models.CharField(max_length=30)
+    Type = models.CharField(max_length=1, choices=TYPECHOICES.choices, default=TYPECHOICES.Student)
+    University = models.CharField(max_length=2, choices=UNIVERSITYCHOICES.choices, default=UNIVERSITYCHOICES.Unknown)
+    Degree = models.CharField(max_length=2, choices=DEGREECHOICES.choices, default=DEGREECHOICES.Unknown)
