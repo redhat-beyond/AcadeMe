17e297ff6dc79158f8bb872136e3b3a2fd990756
DanWaldman <dan.waldman14@gmail.com>
2021-11-14 20:08:15 +0200

    Adding Model file for Forums database - FIXED COMMIT#2
    Files need to be uploaded together

--- AcadeMeData/ForumModels.py
+++ AcadeMeData/ForumModels.py
@@ -1,26 +1,4 @@
-from django.db import models
-from django.conf import settings
-
-
-class DEGREECHOICES(models.TextChoices):
-    Computer_Science = 'CS', 'Computer Science'
-    Psychology = 'PS', 'Psychology'
-    GOVERNMENT = 'GV', 'GOVERNMENT'
-    Business_Administration = 'BA', 'Business Administration'
-    Unknown = 'UN', 'Unknown'
-
-
-class TYPECHOICES(models.TextChoices):
-    Student = 'S', 'Student'
-    Expert = 'E', 'Expert'
-
-
-class UNIVERSITYCHOICES(models.TextChoices):
-    Reichman_University = 'RU', 'Reichman University'
-    Hebrew_University = 'HU', 'Hebrew University'
-    Tel_Aviv_University = 'TA', 'Tel Aviv University'
-    Beer_Sheva_University = 'BS', "Be'er Sheva University"
-    Unknown = 'UN', 'Unknown'
+from UserModels import *
 
 
 class MessageBoards(models.Model):
