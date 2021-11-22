d4927f0097c0ed33995a1563bff0793a99d72607
DanWaldman <dan.waldman14@gmail.com>
2021-11-23 01:11:00 +0200

    Adding migrations files:
    - 0001_initial created automatically by Django
    - 0002_users_test_data created by me with samples of user data

--- AcadeMeData/migrations/0002_users_test_data.py
+++ AcadeMeData/migrations/0002_users_test_data.py
@@ -2,7 +2,6 @@ from django.db import migrations, transaction
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('AcadeMeData', '0001_initial'),
     ]
@@ -12,14 +11,15 @@ class Migration(migrations.Migration):
         from django.contrib.auth.models import User
 
         user_test_data = [
-            ('user1', 'user1@gmail.com', 'password1'),
-            ('User2', 'user2@gmail.com', 'Password2'),
-            ('User3', 'user3@gmail.com', 'Password3'),
-            ('user4', 'user4@gmail.com', 'Password4')
+            ('user1', 'user1@gmail.com', 'password1', 'S', 'BS', 'CS'),
+            ('User2', 'user2@gmail.com', 'Password2', 'E', 'RU', 'PS'),
+            ('User3', 'user3@gmail.com', 'Password3', 'S', 'TA', 'BA'),
+            ('user4', 'user4@gmail.com', 'Password4', 'E', 'BS', 'CD')
         ]
         with transaction.atomic():
-            for username, email, password in user_test_data:
-                user = User.objects.create_user(username=username, email=email, password=password)
+            for username, email, password, type, university, degree in user_test_data:
+                user = User.objects.create_user(username=username, email=email, password=password, type=type,
+                                                university=university, degree=degree)
                 Users(user=user).save()
 
     operations = [
