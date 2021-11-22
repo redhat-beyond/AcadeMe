0adbb1ca41f0393a0dfa2f6ba594539420a1535a
DanWaldman <dan.waldman14@gmail.com>
2021-11-23 01:10:00 +0200

    Added tests file including:
    - test for adding user
    - test for removing user

--- AcadeMeData/tests.py
+++ AcadeMeData/tests.py
@@ -1,3 +1,34 @@
-# from django.test import TestCase
+from django.test import TestCase
+from AcadeMeData.models import Users
 
-# Create your tests here.
+
+class TestUserModel(TestCase):
+
+    def test_create_user(self):
+        username = "username"
+        password = "password"
+        email = "user@example.com"
+        type = "S"
+        university = "RU"
+        degree = "CS"
+        user = Users.create_user(
+            username, email, password, type, university, degree)
+        assert isinstance(user, Users)
+        assert user.user.username == username
+        assert user.user.password == password
+        assert user.user.email == email
+        assert user.user.type == type
+        assert user.user.university == university
+        assert user.user.degree == degree
+
+    def test_del_user(self):
+        username = "username"
+        password = "password"
+        email = "user@example.com"
+        type = "S"
+        university = "RU"
+        degree = "CS"
+        user = Users.create_user(
+            username, email, password, type, university, degree)
+        assert isinstance(user, Users)
+        assert Users.del_user(user)
