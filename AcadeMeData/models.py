e35587aad9bdf0d3de516e1cb70c01527bcbe242
Shaked Hagag <shakedc200@gmail.com>
2021-11-23 20:32:46 +0200

    Added Message class

--- AcadeMeData/models.py
+++ AcadeMeData/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 
 class Message(models.Model):
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT)
     text = models.TextField()
     date = models.DateTimeField(default=timezone.now)
+    
