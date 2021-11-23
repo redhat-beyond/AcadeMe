cafc4443c2c41fd3819161f5211c35a1bdfffd2c
Daniel Itkes <danielitkes@gmail.com>
2021-11-23 17:09:11 +0200

    Added tests for degree model

--- AcadeMeData/tests.py
+++ AcadeMeData/tests.py
@@ -1,3 +1,13 @@
-# from django.test import TestCase
+import pytest
+from AcadeMeData.models import Degree
 
-# Create your tests here.
+
+@pytest
+class TestDegreeModel(pytest):
+    def test_get_name(self):
+        test_degree = Degree(degree='Computer Science').save()
+        assert test_degree.get_name() == 'Reichman University'
+
+    def test_universities(self):
+        test_degree = Degree(degree='Economics', universities='Reichman University').save()
+        assert "Reichman University" in test_degree.universities()
