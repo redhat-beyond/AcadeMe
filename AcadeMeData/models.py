f5d1a9555c9eee748c04266a158f16cf372a61d8
Shaked Hagag <shakedc200@gmail.com>
2021-11-23 21:35:30 +0200

    Added board

--- AcadeMeData/models.py
+++ AcadeMeData/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.db.models import Lookup
 
 
 class Message(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT)
     text = models.TextField()
     date = models.DateTimeField(default=timezone.now)
-    
+    board = models.ForeignKey(
+        LookupValues,
+        limit_choices_to={'category_id': 1},
+        on_delete=models.RESTRICT)
+
