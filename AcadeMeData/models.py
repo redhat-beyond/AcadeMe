0f39bc9bb878d932f5d2cfb9fd57cd3b4f3aaec9
Daniel Itkes <danielitkes@gmail.com>
2021-11-23 17:02:36 +0200

    Added the model for Degree

---
+++ AcadeMeData/models.py
@@ -0,0 +1,32 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django.conf import settings
+
+
+class Degree(models.Model):
+    degree = models.CharField(max_length=100)
+    universities = models.TextField(null=True, blank=True)  # Format should be "Uni1, Uni2, Uni3,..."
+    description = models.TextField(null=True, blank=True)  # Describes the degree
+
+    # methods
+    def __str__(self):
+        """
+        Returns the name of all possible degrees in the database.
+        """
+        return self.degree
+
+    def get_name(self):
+        return self.degree
+
+    def get_description(self):
+        """
+        Returns the description of a specific degree.
+        """
+        return self.description
+
+    def get_universities(self):
+        """
+        Returns a string of all universities that offers this degree.
+        * will be used later to determine if a university offers the specific degree.
+        """
+        return self.universities
