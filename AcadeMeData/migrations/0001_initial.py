762a8b89815436f809d5338a08705910a8936ea5
DanWaldman <dan.waldman14@gmail.com>
2021-11-24 12:53:16 +0200

    Updated migrations

--- AcadeMeData/migrations/0001_initial.py
+++ AcadeMeData/migrations/0001_initial.py
@@ -8,9 +8,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
-    ]
 
     operations = [
         migrations.CreateModel(
--- AcadeMeData/migrations/0006_university_test_data.py
+++ AcadeMeData/migrations/0006_university_test_data.py
@@ -4,9 +4,9 @@ from django.db import migrations, transaction
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '__latest__'),
         ('AcadeMeData', '0001_initial'),
     ]
-
     def generate_university_data(apps, schema_editor):
         from AcadeMeData.models import University
 
