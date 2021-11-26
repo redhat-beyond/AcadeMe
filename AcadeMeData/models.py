1c0b2135ff2ce4d3fde3c2c2d4d3ff5168db1c3e
Daniel Itkes <89524327+DanielItkes@users.noreply.github.com>
2021-11-26 18:36:51 +0200

    Update models.py

--- AcadeMeData/models.py
+++ AcadeMeData/models.py
@@ -68,3 +68,35 @@ class User(models.Model):
             return False
         return user
     """
+
+
+class Degree(models.Model):
+    name = models.CharField(max_length=100)
+    universities = models.TextField(null=True, blank=True)  # Format should be "Uni1, Uni2, Uni3,..."
+    description = models.TextField(null=True, blank=True)  # Describes the degree
+
+    # methods
+    def __str__(self):
+        """
+        Returns the name of all possible degrees in the database.
+        """
+        return self.name
+
+    def get_name(self):
+        return self.name
+        """
+        Returns the name of a specific degree.
+        """
+
+    def get_description(self):
+        """
+        Returns the description of a specific degree.
+        """
+        return self.description
+
+    def get_universities(self):
+        """
+        Returns a string of all universities that offers this degree.
+        * will be used later to determine if a university offers the specific degree.
+        """
+        return self.universities
