0430ba022aa1179f98a6af293892819f4465c0d7
Shaked Hagag <shakedc200@gmail.com>
2021-11-27 14:01:50 +0200

    Finished Messages and MessageBoards and messageTAGS  with migrations!

---
+++ AcadeMeData/migrations/0005_MessageTags_test_data.py
@@ -0,0 +1,24 @@
+from django.db import migrations, transaction
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('auth', '__latest__'),
+        ('AcadeMeData', '0001_initial'),
+    ]
+
+    def generate_msgtagsdata(apps, schema_editor):
+        from AcadeMeData.models import User, Messages, MessageTags
+        userrnd1=User.create_user('u', 'u@gmail.com', 'pas', 'S', 'BS', 'CS')
+        msgID = Messages.create_message(1, userrnd1, 'Hello this is test message')
+        test_data = [
+            (1, msgID ,userrnd1),
+        ]
+        with transaction.atomic():
+            for id,msgID, userID in test_data:
+                msgTag=MessageTags(id=id, msgID=msgID, userID=userID)
+                msgTag.save()
+
+    operations = [
+        migrations.RunPython(generate_msgtagsdata),
+    ]
