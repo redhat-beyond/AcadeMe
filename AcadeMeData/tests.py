12cb985dd51b1017adb09d1aa7ae7d2e5f34dfc2
Daniel Itkes <danielitkes@gmail.com>
2021-12-01 18:44:23 +0200

    fix

--- AcadeMeData/tests.py
+++ AcadeMeData/tests.py
@@ -95,13 +95,12 @@ class TestDegreeModel:
     @pytest.fixture
     def generate_degree(self, degree_id=1, name='History', universities="Ben Gurion University, Reichman University",
                         description="Learn about historic events and their influences on the world"):
-        degree = Degree(degree_id=degree_id, name=name, universities=universities,
-                                description=description)
+        degree = Degree(degree_id=degree_id, name=name, universities=universities, description=description)
         degree.save()
         return degree
 
     def test_create_degree(self):
-        degree = Degree.create_degree(name="History",
+        degree = Degree.create_degree(degree_id=1, name="History",
                                       universities="Reichman University, Ben Gurion University",
                                       description="Learn about historic events and their influences on the world")
 
