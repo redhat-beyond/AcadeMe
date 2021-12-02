813f43148966c49ea53a60a86e35b9272e9aa718
Daniel Itkes <danielitkes@gmail.com>
2021-12-02 10:37:33 +0200

    fix

--- AcadeMeData/test_degree.py
+++ AcadeMeData/test_degree.py
@@ -8,15 +8,15 @@ class TestDegreeModel:
     def generate_degree(self, degree_id=1, name='History', universities="Ben Gurion University, Reichman University",
                         description="Learn about historic events and their influences on the world"):
         degree = Degree(degree_id=degree_id, name=name, universities=universities,
-                                description=description)
+                        description=description)
         degree.save()
         return degree
 
     def test_create_degree(self):
-        degree = create_degree(self, degree_id=1, name="History",
+        degree = Degree.create_degree(self, degree_id=1, name="History",
                                universities="Reichman University, Ben Gurion University",
                                description="Learn about historic events and their influences on the world")
 
         assert "Reichman University" in degree.get_universities()
         assert "History" in degree.get_name
-        assert "historics" in degree.get_description()
+        assert "historic" in degree.get_description()
