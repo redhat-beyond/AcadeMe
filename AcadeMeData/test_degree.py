a916f7aba24921fee4af81ab158420a61567f718
Daniel Itkes <danielitkes@gmail.com>
2021-12-02 10:34:02 +0200

    fix

--- AcadeMeData/test_degree.py
+++ AcadeMeData/test_degree.py
@@ -4,16 +4,19 @@ from AcadeMeData.models import Degree
 
 @pytest.mark.django_db
 class TestDegreeModel:
-    def degree_example(self):
-        degree = Degree(degree_id=1, name='History', universities="Reichman University",
-                        description="Learn about historic events and their influences on the world")
+    @pytest.fixture
+    def generate_degree(self, degree_id=1, name='History', universities="Ben Gurion University, Reichman University",
+                        description="Learn about historic events and their influences on the world"):
+        degree = Degree(degree_id=degree_id, name=name, universities=universities,
+                                description=description)
         degree.save()
         return degree
 
-    def test_create_degree(self, degree_id=1, name="History",
-                           universities="Reichman University, Ben Gurion University",
-                           description="Learn about historic events and their influences on the world"):
-        degree_test = Degree.create_degree(degree_id=degree_id, name=name, universities=universities,
-                                           description=description)
-        assert "Reichman University" in degree_test.universities
-        assert degree_test.name == "History"
+    def test_create_degree(self):
+        degree = create_degree(self, degree_id=1, name="History",
+                               universities="Reichman University, Ben Gurion University",
+                               description="Learn about historic events and their influences on the world")
+
+        assert "Reichman University" in degree.get_universities()
+        assert "History" in degree.get_name
+        assert "historics" in degree.get_description()
