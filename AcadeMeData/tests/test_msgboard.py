85b00b362fb8ff5d5d36cf5295cecdc8177f5a85
Shaked Hagag <76105109+shakedhagag@users.noreply.github.com>
2021-12-03 20:28:12 +0200

    Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.

--- AcadeMeData/tests/test_msgboard.py
+++ AcadeMeData/tests/test_msgboard.py
@@ -5,6 +5,9 @@ from AcadeMeData.models import MessageBoards
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e0ebfd2 (Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.)
 =======
 >>>>>>> e0ebfd2 (Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.)
 @pytest.fixture
@@ -14,6 +17,7 @@ def generate_msgboard(id=1, courseName="Linear Algebra"):
     return msgboard
 
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 @pytest.mark.django_db
 class TestMessageBoardModel:
@@ -36,6 +40,10 @@ class TestMessageBoardModel:
 >>>>>>> e0ebfd2 (Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.)
 =======
 >>>>>>> 74349c9 (Seperated the tests.py file into 3 files each one contains Tests for each model class of messages. Also all of the tests are inside a new folder called Tests.)
+=======
+@pytest.mark.django_db
+class TestMessageBoardModel:
+>>>>>>> e0ebfd2 (Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.)
     def test_get_msgboard(self, generate_msgboard):
         msgboard = generate_msgboard
         msgboard_test = MessageBoards.get_msgboard_by_id(1)
--- AcadeMeData/tests/test_msgtags.py
+++ AcadeMeData/tests/test_msgtags.py
@@ -3,6 +3,9 @@ from AcadeMeData.models import MessageTags, Messages, User
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e0ebfd2 (Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.)
 @pytest.fixture
 def user_example():
     user_data = {'username': "username2212", 'password': "password", 'email': "user@example.com", 'type': "S",
@@ -12,6 +15,7 @@ def user_example():
     return user
 
 
+<<<<<<< HEAD
 @pytest.mark.django_db
 class TestMessageTagsModel:
 =======
@@ -26,6 +30,10 @@ class TestMessageTagsModel:
         return user
 
 >>>>>>> 74349c9 (Seperated the tests.py file into 3 files each one contains Tests for each model class of messages. Also all of the tests are inside a new folder called Tests.)
+=======
+@pytest.mark.django_db
+class TestMessageTagsModel:
+>>>>>>> e0ebfd2 (Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.)
     def test_get_msg_tag(self, user_example, id=1, msgID=None):
         userID = user_example
         msgID = Messages.create_message(id, userID, text='bla bla')
