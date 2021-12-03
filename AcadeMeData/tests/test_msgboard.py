12bb7fa6a90ff88b482708ad3b0b825eb1644a78
Shaked Hagag <76105109+shakedhagag@users.noreply.github.com>
2021-12-03 20:28:12 +0200

    Fixed the last fixtures review, moved them outside of classes for convenience. added some assert functions to make the tests better.

--- AcadeMeData/tests/test_msgboard.py
+++ AcadeMeData/tests/test_msgboard.py
@@ -2,14 +2,15 @@ import pytest
 from AcadeMeData.models import MessageBoards
 
 
+@pytest.fixture
+def generate_msgboard(id=1, courseName="Linear Algebra"):
+    msgboard = MessageBoards(id=id, courseName=courseName)
+    msgboard.save()
+    return msgboard
+
+
 @pytest.mark.django_db
 class TestMessageBoardModel:
-    @pytest.fixture
-    def generate_msgboard(self, id=1, courseName="Linear Algebra"):
-        msgboard = MessageBoards(id=id, courseName=courseName)
-        msgboard.save()
-        return msgboard
-
     def test_get_msgboard(self, generate_msgboard):
         msgboard = generate_msgboard
         msgboard_test = MessageBoards.get_msgboard_by_id(1)
