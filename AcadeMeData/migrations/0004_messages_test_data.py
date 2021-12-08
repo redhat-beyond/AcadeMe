be4a9e7da6e0ee041f4e593fe88569b2fd480e7f
Shaked Hagag <76105109+shakedhagag@users.noreply.github.com>
2021-12-08 19:37:18 +0200

    Migration files for Messages, MessageBoards, MessageTags

---
+++ AcadeMeData/migrations/0004_messages_test_data.py
@@ -0,0 +1,23 @@
+from django.db import migrations, transaction
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('auth', '__latest__'),
+        ('AcadeMeData', '0001_initial'),
+    ]
+
+    def generate_msgdata(apps, schema_editor):
+        from AcadeMeData.models import Messages, User
+        userrnd1 = User.create_user('name', 'email', 'pass', 's', 'kaki', 'cs')
+        test_data = [
+            (1, userrnd1, 'random message'),
+        ]
+        with transaction.atomic():
+            for msgID, userID, text in test_data:
+                msg = Messages(msgID=msgID, userID=userID, text=text)
+                msg.save()
+
+    operations = [
+        migrations.RunPython(generate_msgdata),
+    ]
