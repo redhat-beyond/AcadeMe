1c2cd61bcefd70dc094259119e82858fa9033086
Daniel Itkes <danielitkes@gmail.com>
2021-12-11 14:48:54 +0200

    Added migration files for Course

---
+++ AcadeMeData/migrations/0009_Course_test_data.py
@@ -0,0 +1,39 @@
+from django.db import migrations, transaction
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '__latest__'),
+        ('AcadeMeData', '0001_initial'),
+    ]
+
+    def generate_data(apps, schema_editor):
+        from AcadeMeData.models import Course, Degree, Professor, University
+        
+        university = University(university_id=1, name="Reichman University", location='Herzliya',
+                                description='Very nice university')
+        university.save()
+
+        degree = Degree(degree_id=2, name="Computer Science", universities="Ben Gurion University",
+                        description="Learn about how to write awesome code!")
+        degree.save()
+        
+        professor = Professor(professor_id=3, name="Shimon Shocken", university=university,
+                              description="Nice guy", rate=1)
+        professor.save()
+
+        test_data = [
+            (1, 'Open Source Code Workshop', degree, False, 'Build a Django project from the grounds up!',
+             professor),
+            (2, '16th Century Poetry', degree, True,
+             'Learn about the revolution in poetry in 16th century England', professor),
+        ]
+        with transaction.atomic():
+            for id, name, degree, elective, description, professor in test_data:
+                Course(course_id=id, name=name, degree=degree, elective=elective,
+                       description=description, professor=professor).save()
+
+    operations = [
+        migrations.RunPython(generate_data),
+    ]
