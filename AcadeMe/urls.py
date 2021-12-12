6ccea95f9af3ac660cb23b2b909c5162188baa94
Raz Ashtar <razashtar93@gmail.com>
2021-12-12 20:57:14 +0200

    removed comments, add work on homepage
    removed unnecessary comments from views.py and urls.py
    keep working on homepage.html and homepage.css

--- AcadeMe/urls.py
+++ AcadeMe/urls.py
@@ -19,6 +19,5 @@ from AcadeMe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.landingPage,  name='landing_page'),
     path('', views.homePage,  name='homepage')
     ]
