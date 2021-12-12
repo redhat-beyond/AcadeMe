d1d806a4e409849d646f5a0bb1e7991fc2ec3751
Daniel Itkes <danielitkes@gmail.com>
2021-12-12 19:03:40 +0200

    Add login/logout/home placeholder webpages.

--- AcadeMe/urls.py
+++ AcadeMe/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from AcadeMeData import views as user_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.landingPage,  name='landing_page')
+    path('home/', user_views.home, name='home'),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     ]
