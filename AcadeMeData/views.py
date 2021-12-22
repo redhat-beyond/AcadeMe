53a34d151b254e0a0f39532d6445ad3d1b581375
Raz Ashtar <razashtar93@gmail.com>
2021-12-22 21:01:00 +0200

    resolve conflicts

--- AcadeMeData/views.py
+++ AcadeMeData/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from AcadeMeData.models import University, Degree
 
 
 def register(request):
@@ -16,3 +17,17 @@ def register(request):
 
     context = {'form': form}
     return render(request, '../templates/registration/registration.html', context)
+
+
+def universities_degrees(request):
+    all_universities = University.objects.all()
+    all_degrees = Degree.objects.all()
+    #     University.get_universities() # added this function in models.py
+    context = {'all_universities': all_universities, 'all_degrees': all_degrees}
+    return render(request, '../templates/landing/homepage.html', context)
+
+# def degrees(request):
+#     all_degrees = Degree.objects.all()
+#
+#     context = {'all_degrees': all_degrees}
+#     return render(request, '../templates/landing/homepage.html', context)
