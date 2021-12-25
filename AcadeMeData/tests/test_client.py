bdb3dd2c37d175f73e59c7c274fb062c0181046f
DanWaldman <dan.waldman14@gmail.com>
2021-12-25 20:19:41 +0200

    Fixed client tests/test_client

--- AcadeMeData/tests/test_client.py
+++ AcadeMeData/tests/test_client.py
@@ -1,13 +1,18 @@
 from AcadeMe import views
+from django.template.loader import render_to_string
 
 
 def test_homepage(client):
     request = client.get('')
     response = views.homePage(request)
+    expected_html = render_to_string('landing/homepage.html', request=request)
     assert response.status_code == 200
+    assert response.content.decode() == expected_html
 
 
 def test_contactus(client):
     request = client.get('contact_us/')
     response = views.contact(request)
+    expected_html = render_to_string('landing/contact_us.html', request=request)
     assert response.status_code == 200
+    assert response.content.decode() == expected_html
