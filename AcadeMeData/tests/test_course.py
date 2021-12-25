86e6e91280788c8d15684d1183b33f9f0fb4d559
DanWaldman <dan.waldman14@gmail.com>
2021-12-25 19:30:33 +0200

    Added test for new foreign key and removed fixtures that are in conftest

--- AcadeMeData/tests/test_course.py
+++ AcadeMeData/tests/test_course.py
@@ -3,12 +3,10 @@ from AcadeMeData.models import Course
 
 
 @pytest.fixture
-def generate_course(generate_degree, generate_professor, course_id=1, name="History of Countries",
+def generate_course(generate_degree, generate_professor, generate_university, course_id=1, name="History of Countries",
                     mandatory=True, description="Learn about historic events and their influences on countries"):
-    professor = generate_professor
-    degree = generate_degree
-    course = Course.create_course(course_id=course_id, name=name, degree=degree, mandatory=mandatory,
-                                  description=description, professor=professor)
+    course = Course.create_course(course_id=course_id, name=name, degree=generate_degree, mandatory=mandatory,
+                                  description=description, professor=generate_professor, university=generate_university)
     course.save()
     return course
 
@@ -17,6 +15,13 @@ def generate_course(generate_degree, generate_professor, course_id=1, name="Hist
 class TestCourseModel:
     def test_get_course_by_name(self, generate_course, name="History of Countries"):
         course = Course.get_course_by_name(name)
-
         assert course.name == generate_course.name
         assert course.professor == generate_course.professor
+
+    def test_if_course_belongs(self, generate_course, generate_degree, generate_university):
+        bool = generate_course.course_belongs(generate_university, generate_degree)
+        assert bool
+
+    def test_if_course_not_belongs(self, generate_course, generate_university):
+        bool = generate_course.course_belongs(generate_university, None)
+        assert not bool
