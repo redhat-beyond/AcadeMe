7974d4852b026854412b115f732c5d7fc7fc49a7
DanWaldman <dan.waldman14@gmail.com>
2021-12-26 10:53:01 +0200

    Split tests

--- AcadeMeData/tests/test_client.py
+++ AcadeMeData/tests/test_client.py
@@ -5,14 +5,24 @@ from django.template.loader import render_to_string
 def test_homepage(client):
     request = client.get('')
     response = views.homePage(request)
-    expected_html = render_to_string('landing/homepage.html', request=request)
     assert response.status_code == 200
+
+
+def test_homepage_html(client):
+    request = client.get('')
+    response = views.homePage(request)
+    expected_html = render_to_string('landing/homepage.html', request=request)
     assert response.content.decode() == expected_html
 
 
 def test_contactus(client):
     request = client.get('contact_us/')
     response = views.contact(request)
-    expected_html = render_to_string('landing/contact_us.html', request=request)
     assert response.status_code == 200
+
+
+def test_contactus_html(client):
+    request = client.get('contact_us/')
+    response = views.contact(request)
+    expected_html = render_to_string('landing/contact_us.html', request=request)
     assert response.content.decode() == expected_html
