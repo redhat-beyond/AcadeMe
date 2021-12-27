9a84de68924e42d6c3990187bf152343f00868d8
Daniel Itkes <danielitkes@gmail.com>
2021-12-27 17:55:30 +0200

    fix

--- AcadeMeData/tests/test_client.py
+++ AcadeMeData/tests/test_client.py
@@ -1,13 +1,14 @@
-import pytest
 from AcadeMe import views
 
 
-@pytest.fixture
 def test_homepage(client):
-    request = client.get('')
-    response = views.homePage(request)
     assert response.status_code == 200
 
+def test_homepage_html(client):
+    request = client.get('')
+    response = views.homePage(request)
+    expected_html = render_to_string('landing/homepage.html', request=request)
+    assert response.content.decode() == expected_html
 
 def test_contactus(client):
     request = client.get('contact_us/')
