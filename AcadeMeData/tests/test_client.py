2074f9967a8826b680b242038e83166101fc891f
DanWaldman <dan.waldman14@gmail.com>
2021-12-27 21:45:10 +0200

    Fixed client tests to be only about http response

--- AcadeMeData/tests/test_client.py
+++ AcadeMeData/tests/test_client.py
@@ -1,28 +1,13 @@
-from AcadeMe import views
-from django.template.loader import render_to_string
+import pytest
 
 
+@pytest.mark.django_db
 def test_homepage(client):
-    request = client.get('')
-    response = views.homePage(request)
+    response = client.get('')
     assert response.status_code == 200
 
 
-def test_homepage_html(client):
-    request = client.get('')
-    response = views.homePage(request)
-    expected_html = render_to_string('landing/homepage.html', request=request)
-    assert response.content.decode() == expected_html
-
-
+@pytest.mark.django_db
 def test_contactus(client):
-    request = client.get('contact_us/')
-    response = views.contact(request)
+    response = client.get('/contact_us/')
     assert response.status_code == 200
-
-
-def test_contactus_html(client):
-    request = client.get('contact_us/')
-    response = views.contact(request)
-    expected_html = render_to_string('landing/contact_us.html', request=request)
-    assert response.content.decode() == expected_html
