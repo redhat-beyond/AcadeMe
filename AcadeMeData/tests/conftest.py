360ae193735959c1e095fa6f9ed87956c78f3440
Shaked Hagag <76105109+shakedhagag@users.noreply.github.com>
2021-12-27 17:36:23 +0200

    Update conftest.py

--- AcadeMeData/tests/conftest.py
+++ AcadeMeData/tests/conftest.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pytest
 from AcadeMeData.models import MessageTags, Messages, University, Degree, Professor, User, MessageBoards, Course
 
@@ -78,3 +79,72 @@ def generate_message(generate_msgboard, user_example, msgID=9, text='Hello this 
 def generate_msgtags(user_example, generate_message, id=1):
     tag = MessageTags.create_msgtag(id, generate_message, user_example)
     return tag
+=======
+import pytest
+<<<<<<< HEAD:AcadeMeData/tests/conftest.py
+from AcadeMeData.models import University, Degree, Professor, User
+=======
+from AcadeMeData.models import University, Degree, Professor, User, Course, MessageBoards
+>>>>>>> 7ac4ac8 (Improved the tests for the msgboard feature including adding fixtures to conftest.):AcadeMeData/conftest.py
+
+
+@pytest.fixture
+def generate_university(university_id=5, name='The Technion', location="Haifa",
+                        description="Best University in Israel"):
+    university = University(university_id=university_id, name=name, location=location,
+                            description=description)
+    university.save()
+    return university
+
+
+@pytest.fixture
+def generate_degree(degree_id=1, name='History101', universities="Ben Gurion University, Reichman University",
+                    description="Learn about historic events and their influences on the world"):
+    degree = Degree.create_degree(degree_id=degree_id, name=name, universities=universities,
+                                  description=description)
+    degree.save()
+    return degree
+
+
+@pytest.fixture
+def generate_professor(generate_university, professor_id=2, name="DR Arnold Schwarzenegger",
+                       description="A cool guy who looked familiar", rate=4.5):
+    professor = Professor.create_professor(professor_id=professor_id,
+                                           name=name,
+                                           university=generate_university,
+                                           description=description,
+                                           rate=rate)
+    return professor
+
+
+@pytest.fixture
+def user_example():
+<<<<<<< HEAD:AcadeMeData/tests/conftest.py
+    user_data = {'username': "username", 'password': "password", 'email': "user@example.com",
+                 'university': "RU", 'degree': "CS"}
+    user = User.create_user(*user_data)
+    return user
+=======
+    user_data = {'username': "username2212", 'password': "password", 'email': "user@example.com",
+                 'university': "RU",
+                 'degree': "CS"}
+    user = User.create_user(*user_data)
+    return user
+
+
+@pytest.fixture
+def generate_course(generate_degree, generate_professor, course_id=1, name="History of Countries",
+                    mandatory=True, description="Learn about historic events and their influences on countries"):
+    course = Course.create_course(course_id=course_id, name=name, degree=generate_degree, mandatory=mandatory,
+                                  description=description, professor=generate_professor)
+    course.save()
+    return course
+
+
+@pytest.fixture
+def generate_msgboard(generate_course, id=1):
+    msgboard = MessageBoards(id=id, courseName='Linear Algebra')
+    msgboard.save()
+    return msgboard
+>>>>>>> 7ac4ac8 (Improved the tests for the msgboard feature including adding fixtures to conftest.):AcadeMeData/conftest.py
+>>>>>>> dfccc46 (Update conftest.py)
