03ddd180df3686e224877e1a772c7faaa952b70a
Shaked Hagag <76105109+shakedhagag@users.noreply.github.com>
2021-12-27 17:30:30 +0200

    Improved the tests for the msgboard feature including adding fixtures to conftest.

---
+++ AcadeMeData/conftest.py
@@ -0,0 +1,56 @@
+import pytest
+from AcadeMeData.models import University, Degree, Professor, User, Course, MessageBoards
+
+
+@pytest.fixture
+def generate_university(university_id=5, name='The Technion', location="Haifa",
+                        description="Best University in Israel"):
+    university = University(university_id=university_id, name=name, location=location,
+                            description=description)
+    university.save()
+    return university
+
+
+@pytest.fixture
+def generate_degree(degree_id=1, name='History', universities="Ben Gurion University, Reichman University",
+                    description="Learn about historic events and their influences on the world"):
+    degree = Degree.create_degree(degree_id=degree_id, name=name, universities=universities,
+                                  description=description)
+    degree.save()
+    return degree
+
+
+@pytest.fixture
+def generate_professor(generate_university, professor_id=2, name="DR Arnold Schwarzenegger",
+                       description="A cool guy who looked familiar", rate=4.5):
+    professor = Professor.create_professor(professor_id=professor_id,
+                                           name=name,
+                                           university=generate_university,
+                                           description=description,
+                                           rate=rate)
+    return professor
+
+
+@pytest.fixture
+def user_example():
+    user_data = {'username': "username2212", 'password': "password", 'email': "user@example.com",
+                 'university': "RU",
+                 'degree': "CS"}
+    user = User.create_user(*user_data)
+    return user
+
+
+@pytest.fixture
+def generate_course(generate_degree, generate_professor, course_id=1, name="History of Countries",
+                    mandatory=True, description="Learn about historic events and their influences on countries"):
+    course = Course.create_course(course_id=course_id, name=name, degree=generate_degree, mandatory=mandatory,
+                                  description=description, professor=generate_professor)
+    course.save()
+    return course
+
+
+@pytest.fixture
+def generate_msgboard(generate_course, id=1):
+    msgboard = MessageBoards(id=id, courseName=generate_course)
+    msgboard.save()
+    return msgboard
--- AcadeMeData/tests/test_msgboard_feature.py
+++ AcadeMeData/tests/test_msgboard_feature.py
@@ -1,21 +1,35 @@
 import pytest
-from AcadeMeData.models import MessageBoards, Course, Degree, Professor, User
 from AcadeMeData.forms import MessageForm
-
-
-@pytest.fixture
-def user_example():
-    user_data = {'username': "username2212", 'password': "password", 'email': "user@example.com",
-                 'university': "RU",
-                 'degree': "CS"}
-    user = User.create_user(*user_data)
-    return user
+from django.contrib.messages import get_messages
 
 
 @pytest.mark.django_db
-def test_msgboard_feature(client, user_example):
+def test_msgboard_login(client, user_example):
     client.force_login(user_example.user)
     response = client.get("/msgboard/")
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_msgboard_form_instance(client, user_example):
+    client.force_login(user_example.user)
+    response = client.get("/msgboard/")
     form = response.context["form"]
     assert isinstance(form, MessageForm)
+
+
+@pytest.mark.django_db
+def test_msgboard_empty_message_when_page_loads(client, user_example):
+    client.force_login(user_example.user)
+    response = client.get("/msgboard/")
+    messages = list(get_messages(response.wsgi_request))
+    assert len(messages) == 0
+
+
+@pytest.mark.django_db
+def test_msgboard_feature(client, user_example, generate_msgboard):
+    form_data = {'text': 'Lorem Ipsum',
+                 'userID': user_example.user,
+                 'board': generate_msgboard}
+    form = MessageForm(data=form_data)
+    assert (form.is_valid())
