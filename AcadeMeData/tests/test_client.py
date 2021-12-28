b42c68a1f2a1f4d76141701c68303e6208a1a4c2
Daniel Itkes <danielitkes@gmail.com>
2021-12-28 12:21:01 +0200

    commit

--- AcadeMeData/tests/test_client.py
+++ AcadeMeData/tests/test_client.py
@@ -1,12 +1,13 @@
 from AcadeMe import views
 
 
+@pytest.mark.django_db
 def test_homepage(client):
     request = client.get('')
     response = views.homePage(request)
     assert response.status_code == 200
 
-
+@pytest.mark.django_db
 def test_contactus(client):
     request = client.get('contact_us/')
     response = views.contact(request)
