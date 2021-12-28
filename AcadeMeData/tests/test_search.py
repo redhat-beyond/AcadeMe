faf9ae5ae405340fed48cd24f719652aa6187cc8
Daniel Itkes <danielitkes@gmail.com>
2021-12-28 07:50:10 +0200

    added tests

--- AcadeMeData/tests/test_search.py
+++ AcadeMeData/tests/test_search.py
@@ -8,8 +8,9 @@ def test_query(generate_course):
     assert Course.objects.filter(name__icontains=generate_course.name)[0].name == generate_course.name
 
 @pytest.mark.django_db
-def test_search(client):
-    response = client.get('/search/')
+def test_search(client, generate_course):
+    url = '{url}?{filter}={value}'.format(url=reverse('search'),filter='q', value=generate-course.name)
+    response = client.get(url)
     assert response.status_code == 200
 
 @pytest.mark.django_db
