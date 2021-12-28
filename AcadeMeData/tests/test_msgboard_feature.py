8a67086af1c8c9d940dc0cc49a0a6476f75b1066
Shaked Hagag <76105109+shakedhagag@users.noreply.github.com>
2021-12-28 09:41:32 +0200

    Added the requested test for the length

--- AcadeMeData/tests/test_msgboard_feature.py
+++ AcadeMeData/tests/test_msgboard_feature.py
@@ -22,15 +22,19 @@ def test_msgboard_form_instance(client, user_example):
 @pytest.mark.django_db
 def test_msgboard_empty_message_when_page_loads(client, user_example):
     client.force_login(user_example.user)
-    response = client.get("/msgboard/")
+    response = client.get("/msgboard")
     messages = list(get_messages(response.wsgi_request))
     assert len(messages) == 0
 
 
 @pytest.mark.django_db
 def test_msgboard_feature(client, user_example, generate_msgboard):
+    client.force_login(user_example.user)
     form_data = {'text': 'Lorem Ipsum',
                  'userID': user_example.user,
                  'board': generate_msgboard}
     form = MessageForm(data=form_data)
+    response = client.post("/msgboard/", form_data)
     assert (form.is_valid())
+    messages = list(response.context['messages'][0].text)
+    assert len(messages) > 0
