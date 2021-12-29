7344f1d50fba422db9bf170987fa0099f0d6d5b0
Dean Civlin <dean.civlin@ILJLLMQVMD6Q.EU.jllnet.com>
2021-12-29 02:02:52 +0200

    fixed flake8 errors

--- AcadeMeData/tests/test_client.py
+++ AcadeMeData/tests/test_client.py
@@ -12,11 +12,13 @@ def test_contactus(client):
     response = client.get('/contact-us/')
     assert response.status_code == 200
 
+
 @pytest.mark.django_db
 def test_courselist(client):
     response = client.get('/course-list/')
     assert response.status_code == 200
-    
+
+
 @pytest.mark.django_db
 def test_coursepage(client):
     response = client.get('/course-page/')
