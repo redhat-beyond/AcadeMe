5f2ae40cd5eddf09918e32a908711adfd18b71c7
Shaked Hagag <76105109+shakedhagag@users.noreply.github.com>
2022-01-01 20:33:08 +0200

    Connected between courses and their msgboards

--- AcadeMeData/tests/test_msgboard_feature.py
+++ AcadeMeData/tests/test_msgboard_feature.py
@@ -7,14 +7,14 @@ from django.contrib import auth
 @pytest.mark.django_db
 def test_msgboard_login(client, user_example):
     client.force_login(user_example.user)
-    response = client.get("/msgboard/")
+    response = client.get("/msgboard/?course_id=1")
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_msgboard_form_instance(client, user_example):
     client.force_login(user_example.user)
-    response = client.get("/msgboard/")
+    response = client.get("/msgboard/?course_id=1")
     form = response.context["form"]
     assert isinstance(form, MessageForm)
 
@@ -24,7 +24,7 @@ def test_msgboard_empty_message_when_page_loads(client, user_example):
     client.force_login(user_example.user)
     user = auth.get_user(client)
     assert user.is_authenticated
-    response = client.get("/msgboard")
+    response = client.get("/msgboard/?course_id=1")
     messages = list(get_messages(response.wsgi_request))
     assert len(messages) == 0
 
@@ -38,7 +38,7 @@ def test_msgboard_feature(client, user_example, generate_msgboard):
                  'userID': user_example.user}
     form = MessageForm(data=form_data)
     assert (form.is_valid())
-    response = client.post("/msgboard/", data=form_data, follow=True)
+    response = client.post("/msgboard/?course_id=1", data=form_data, follow=True)
     form2 = response.context["form"]
     assert isinstance(form2, MessageForm)
     assert response.status_code == 200
